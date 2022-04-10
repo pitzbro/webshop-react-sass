38e7a34b6a4a29a45f05af6b90cf228321f67eee
pitzbro <asioren@gmail.com>
2022-04-10 19:27:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header';
+import News from './components/News';
+import MainText from './components/MainText';
+import Cards from './components/Cards';
+import Sidebar from './components/Sidebar';
+
+import './styles/styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="main-layout">
+      <Sidebar />
+      <Header />
+      <News />
+      <MainText />
+      <Cards />
     </div>
   );
 }
